d22335ad328e7fef5b15ed8199e5f63250cf0395
Abha Zodape <azodape@copyright.com>
2015-05-13 16:40:12 +0530

    Update functionLibrary.java

--- src/test/java/Utilities/functionLibrary.java
+++ src/test/java/Utilities/functionLibrary.java
@@ -3,7 +3,14 @@ package Utilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.JavascriptExecutor;
+
+
 public class functionLibrary {
+	
+	/**
+         * To maximize the browser.
+	 * @param driver
+	 */
  
 	public static void maximizeWindow(WebDriver driver){
 		
@@ -11,6 +18,10 @@ public class functionLibrary {
 		
 	}
 	
+	/**
+	 * @param driver is the firefox driver.
+	 * @param element is web element to be scrolled.
+	 */
 	public static void scrollTo(WebDriver driver, WebElement element) {
         ((JavascriptExecutor) driver).executeScript(
                 "arguments[0].scrollIntoView();", element);
